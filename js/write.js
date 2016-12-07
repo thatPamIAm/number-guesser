73eb1ce723ef404dd03a56a7af25d11b862b4e57
Pamela Lovett <pamela.s.lovett@gmail.com>
2016-12-07 12:57:02 -0700

    Change h1. Add event listeners.

--- js/write.js
+++ js/write.js
@@ -9,7 +9,17 @@ var resButton = document.querySelector('.res-button');
 
 var ranNum = Math.floor(Math.random() * 100 + 1);
 
+clearButton.addEventListener('click',function(){
+  console.log('PIZZZA');
+  // document.getElementById('guess-field').value;
+  clearMe();
+});
 
+
+function clearMe() {
+  return userGuess.value= "";
+};
+///test
 // if (answer < randomNum) - display "That is too low"
 // if(answer > randomNum) - display "That is too high"
 // if (answer == randomNum) - display "BOOM!"
