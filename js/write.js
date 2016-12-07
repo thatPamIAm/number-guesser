d125bac9cdc71a98d30de42293cc2747986a8a19
Pamela Lovett <pamela.s.lovett@gmail.com>
2016-12-06 21:15:36 -0700

    Add styles to box. Add vars to JS file. Begin work on function for generator.

--- js/write.js
+++ js/write.js
@@ -1,5 +1,17 @@
-//function for taking input from guess and
-//displaying ind div box
+var guess = document.getElementById('guess-field');
+var box = document.getElementById('box');
+var submitGuessButton = document.querySelector('.guess-button');
+var SubmitClearButton = document.querySelector('.clear-button');
+var SubmitResetButton = document.querySelector('.reset-button');
+
+submitGuessButton.addEventListener('click', function(){
+  var userGuess =
+  box.innerText = guess.value;
+})
+// if (answer < randomNum) - display "That is too low"
+// if(answer > randomNum) - display "That is too high"
+// if (answer == randomNum) - display "BOOM!"
+// return false;
 
 //function for clearing fieldset
 
